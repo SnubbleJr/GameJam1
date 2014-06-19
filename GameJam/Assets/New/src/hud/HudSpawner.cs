aa9eabbd5c7532ba03b719c813a92847b3e1c3e0
Edward James <Edwards_fun@hotmail.com>
2014-06-19 17:40:38 +0100

    Ported space game to c#
    GUI is still not working, and for some reason character moves to centre
    b efore being controllable

---
+++ GameJam/Assets/New/src/hud/HudSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudSpawner : MonoBehaviour {
+
+    public GameObject theHud, theScores;
+
+	// Use this for initialization
+    void Start()
+    {
+        Instantiate(theHud, new Vector3(0.5f, 0.5f, 0f), Quaternion.identity); 
+	    GameObject scores = Instantiate(theScores, new Vector3(0.5f, 0.5f, 0f), Quaternion.identity) as GameObject;
+	    C_BonusManager player = Utils.getTopmostParent(gameObject).GetComponentInChildren<C_BonusManager>();
+	    player.setScoreBoard(scores.GetComponent<ScoreDisplay>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GameJam/Assets/New/src/server/Camera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (Network.isClient)
+        {
+            enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
